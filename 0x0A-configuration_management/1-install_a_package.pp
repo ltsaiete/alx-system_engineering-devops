543641f7678397b210d24c8388e14e229826d34b
Luis Saiete <luistsaiete@gmail.com>
2023-07-05 23:11:52 +0200

    Fix task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # install flask from pip3
 
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  refreshonly => true,
+package{'flask':
+  ensure   =>  '2.1.0',
+  provider =>  'pip3'
 }
