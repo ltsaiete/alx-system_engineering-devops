ea7b6968bf8885eab0bccd34d06d06fd7de86d61
Luis Saiete <luistsaiete@gmail.com>
2023-07-08 09:16:18 +0200

    Use path /etc/ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,8 @@
 #  set up your client SSH configuration file so that you can
 # connect to a server without typing a password.
 
-file {  '~/.ssh/config':
+file {  '/etc/ssh/ssh_config':
   ensure  => 'present',
-  path    => '~/.ssh/config',
-  content => "Host 54.84.66.149\n
-                \tIdentityFile ~/.ssh/school
-                \tPasswordAuthentication no"
+  path    => '/etc/ssh/ssh_config',
+  content => "Host 54.84.66.149\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no"
 }
