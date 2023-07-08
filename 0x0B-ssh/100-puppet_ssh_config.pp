889c2ecc31946a630f17222f3b910fa13ea6cad0
Luis Saiete <luistsaiete@gmail.com>
2023-07-08 09:09:21 +0200

    Make file executable

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,6 @@
 #  set up your client SSH configuration file so that you can
 # connect to a server without typing a password.
+
 file {  '~/.ssh/config':
   ensure  => 'present',
   path    => '~/.ssh/config',
