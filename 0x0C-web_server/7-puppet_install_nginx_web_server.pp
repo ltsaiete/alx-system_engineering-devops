f8e18e96322cba42518349bb711aa4e7dc829dfc
Luis Saiete <luistsaiete@gmail.com>
2023-07-17 22:40:57 +0200

    Fix task 4

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,30 @@
 # configure an Nginx server using Puppet
 
+exec { 'install_puppet_nginx_module':
+  command     => 'puppet module install puppet-nginx',
+  path        => '/usr/local/bin:/usr/bin:/bin',
+  unless      => 'puppet module list --modulepath /etc/puppetlabs/code/modules\
+   | grep puppet-nginx',
+  require     => Class['puppet_agent'],
+  refreshonly => true,
+}
+
 class { 'nginx':
 }
 
 file { '/var/www/html/index.html':
   ensure  => 'file',
   path    => '/var/www/html/index.html',
-  content => '<html>\
-<head>\
-  <title>Document</title>\
-</head>\
+  content => '<html>
+<head>
+  <title>Document</title>
+</head>
 <body>Hello World!</body>
 </html>',
+}
+
+nginx::resource { 'default':
+  port => 80
 }
 
 nginx::resource { 'redirection':
@@ -20,5 +33,3 @@ nginx::resource { 'redirection':
   index    => 'index.html',
   rewrite  => '^/redirect_me https://youtu.be/F1RrJ7DfDMg permanent',
 }
-
-
