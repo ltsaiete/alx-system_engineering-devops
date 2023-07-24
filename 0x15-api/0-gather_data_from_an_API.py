511b4032f1e0aec5174c46df5588502aa52d11e0
Luis Saiete <luistsaiete@gmail.com>
2023-07-24 23:47:02 +0200

    Verify args

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 
 """using this REST API, for a given employee ID,
-   returns information about his/her TODO list progress.
+returns information about his/her TODO list progress.
 """
 import requests
 import sys
+if len(sys.argv) > 1:
+    id = sys.argv[1]
 
-id = sys.argv[1]
+    r = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={id}')
+    todos = r.json()
+    r = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    user = r.json()
 
-r = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={id}')
-todos = r.json()
-r = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
-user = r.json()
+    done = 0
+    total = len(todos)
 
-done = 0
-total = len(todos)
+    for todo in todos:
+        if todo['completed']:
+            done = done + 1
 
-for todo in todos:
-    if todo['completed']:
-        done = done + 1
+    print(f'Employee {user["name"]} is done with tasks({done}/{total}):')
 
-print(f'Employee {user["name"]} is done with tasks({done}/{total}):')
-
-for todo in todos:
-    if todo['completed']:
-        print(f'\t{todo["title"]}')
+    for todo in todos:
+        if todo['completed']:
+            print(f'\t{todo["title"]}')
