0872ce3ad52f1187fc6bf731c9c60e1aa83d5221
Luis Saiete <luistsaiete@gmail.com>
2023-07-25 00:05:27 +0200

    Print usernames

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,11 +14,8 @@ if len(sys.argv) > 1:
     r = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
     user = r.json()
 
-    for todo in todos:
-        if todo['completed']:
-            print(f'\t {todo["title"]}')
     with open(f'{id}.csv', 'w') as f:
         writer = csv.writer(f)
-        row = [str(id), user["name"], todo["completed"], todo["title"]]
+        row = [str(id), user["username"], todo["completed"], todo["title"]]
         for todo in todos:
             writer.writerow(row)
