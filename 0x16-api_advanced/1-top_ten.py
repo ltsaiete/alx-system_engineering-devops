016451734cee2513ce6a3a8f18f214bc2a9caa16
Luis Saiete <luistsaiete@gmail.com>
2023-08-09 22:15:55 +0200

    print top 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a simple module and it only has
+one function called top_ten
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """this function queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): subreddit
+    """
+    headers = {'User-Agent': 'Reddit API calls by Lewis'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        posts = r.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
